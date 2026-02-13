e7ab0f7040a2cf0691a95688250812a84deb9190
Henrique <henrique@email.com>
2026-02-13 17:29:29 -0300

    fix: corrige sintaxe do get projects

--- apps/web/src/http/get-projects.ts
+++ apps/web/src/http/get-projects.ts
@@ -20,7 +20,7 @@ interface GetProjectsResponse {
 
 export async function getProjects(org: string) {
      const result = await api
-    .get(`organizations/${org}/projects`)
+    .get(`organizations/${org}/projects`) 
     .json<GetProjectsResponse>()
 
     return result
